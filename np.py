2f8b52334bfde9a99aa80edd1d9229fc71fe5dd2
gregist <grigori.sun@gmail.com>
2015-05-16 22:54:29 +0800

    test

--- np.py
+++ np.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-x = raw_input('post name:')
+# x = raw_input('post name:')
 title = raw_input('title:')
-f = open('_posts/'+x+'.md','w')
-time=str(datetime.now())[:19]
-f.write('---\nlayout:     post\ntitle:      '+title+'\ndate:       '+time+'\nsummary:    \ncategories: drawing\n---')
+time=str(datetime.now())
+f = open('_posts/'+time[:10]+'-'+title+'.md','w')
+
+f.write('---\nlayout:     post\ntitle:      '+title+'\ndate:       '+time[:19]+'\nsummary:    \ncategories: drawing\n---')
 f.close

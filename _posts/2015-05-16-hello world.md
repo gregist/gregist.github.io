804c82f22d99b54c7bda2e09b57d52dcd76d0bc3
gregist <grigori.sun@gmail.com>
2015-05-16 23:36:25 +0800

    test

--- _posts/2015-05-16-hello world.md
+++
@@ -1,9 +0,0 @@
----
-layout:     post
-title:      hello world
-date:       2015-05-16 22:54:03
-summary:    
-categories: drawing
----
-
-test
--- np.py
+++ np.py
@@ -1,8 +1,13 @@
+import re
 from datetime import datetime
+def urlify(s):
+	s = re.sub(r"[^\w\s]", '', s)
+	s = re.sub(r"\s+", '-', s)
+	return s
 # x = raw_input('post name:')
 title = raw_input('title:')
 time=str(datetime.now())
-f = open('_posts/'+time[:10]+'-'+title+'.md','w')
+f = open('_posts/'+time[:10]+'-'+urlify(title)+'.md','w')
 
 f.write('---\nlayout:     post\ntitle:      '+title+'\ndate:       '+time[:19]+'\nsummary:    \ncategories: drawing\n---')
 f.close

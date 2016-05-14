d768f0e0356c8f2782cdcf8c95935b67f57c7308
gregist <grigori.sun@gmail.com>
2016-05-14 22:33:50 +0800

    rev

--- dr.py
+++ dr.py
@@ -10,7 +10,7 @@ def urlify(string):
     string = re.sub(r"\s+", '-', string)
     return string
 
-INPUT = input('title: ').split('|')
+INPUT = raw_input('title: ').split('|')
 TITLE = INPUT[0]
 CONTEXT = INPUT[1] if len(INPUT)>1 else ''
 

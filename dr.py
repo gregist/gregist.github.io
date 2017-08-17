7c9e65fa01d9e20df00397b8e11204b3f7ab105c
gregist <grigori.sun@gmail.com>
2017-08-18 01:37:25 +0800

    rev

--- dr.py
+++ dr.py
@@ -4,21 +4,25 @@ import re
 from datetime import datetime
 from datetime import timedelta
 
+
 def urlify(string):
     '''urlify'''
     string = re.sub(r"[^\w\s]", '', string)
     string = re.sub(r"\s+", '-', string)
     return string
 
+# INPUT = '~2016-07-17|'
 INPUT = input('title: ').split('|')
 TITLE = INPUT[0]
-CONTEXT = INPUT[1] if len(INPUT)>1 else ''
+CONTEXT = INPUT[1] if len(INPUT) > 1 else ''
 
 TIME = datetime.now()
-while TITLE[:1]=='`':
+while TITLE[:1] == '`':
     TIME = TIME - timedelta(hours=24)
-    TITLE=TITLE[1:]
+    TITLE = TITLE[1:]
 TIME = str(TIME)
+
+print(TIME)
 
 CONTENT = '''---
 layout:     post
@@ -28,8 +32,7 @@ summary:
 categories: drawing
 ---
 ![%s](/images/diary/%s.png \"%s\")
-'''%(TITLE, TIME[:19], TITLE, urlify(TITLE),CONTEXT)
+''' % (TITLE, TIME[:19], TITLE, urlify(TITLE), CONTEXT)
 
 with open('_posts/'+TIME[:10]+'-'+urlify(TITLE)+'.md', 'w') as flw:
     flw.write(CONTENT)
-
